1cf4fb2d56cdb96bf263a24eeb38faa650189ed8
Rodney Woollett <rodney.woollett@gmail.com>
2022-01-08 09:45:25 +1300

    UpdateFormPane

---
+++ src/components/updates/UpdateFormPane.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+
+interface IUpdateFormPaneProps {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+const UpdateFormPane = (props: IUpdateFormPaneProps) => {
+  const { isOpen, handleClose } = props;
+  const [ content, setContent ] = useState('');
+
+  const handleSubmit = (evt: React.ChangeEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    alert(content);
+  };
+
+  return (
+    <SlidingPane
+      className="some-custom-class"
+      overlayClassName="some-custom-overlay-class"
+      isOpen={isOpen}
+      title={content ? content : "Hey"}
+      subtitle="Optional subtitle."
+      onRequestClose={handleClose}
+    >
+    <form onSubmit={handleSubmit}>
+      <input 
+        value={content}
+        onChange={(evt) => setContent(evt.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  </SlidingPane>
+  );
+};
+
+export default UpdateFormPane;
